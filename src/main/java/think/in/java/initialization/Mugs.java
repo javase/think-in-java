7f546532cddf76ddca1eb8a35a857f60723e3b4b
limenghua <android.hua@gmail.com>
2017-06-04 10:51:36 +0800

    静态和非静态实例初始化

---
+++ src/main/java/think/in/java/initialization/Mugs.java
@@ -0,0 +1,53 @@
+package think.in.java.initialization;//: initialization/Mugs.java
+// Java "Instance Initialization."
+
+import static think.in.java.common.Print.*;
+
+class Mug {
+    Mug(int marker) {
+        print("Mug无参构造器(" + marker + ")");
+    }
+
+    void f(int marker) {
+        print("f(" + marker + ")");
+    }
+}
+
+public class Mugs {
+    Mug mug1;
+    Mug mug2;
+
+    {
+        mug1 = new Mug(1);
+        mug2 = new Mug(2);
+        print("mug1 & mug2 initialized");
+    }
+
+    Mugs() {
+        print("Mugs无参构造器()");
+    }
+
+    Mugs(int i) {
+        print("Mugs一参构造器(int)");
+    }
+
+    public static void main(String[] args) {
+        print("Inside main()");
+        new Mugs();
+        print("new Mugs() completed");
+        new Mugs(1);
+        print("new Mugs(1) completed");
+    }
+} /* Output:
+Inside main()
+Mug(1)
+Mug(2)
+mug1 & mug2 initialized
+Mugs()
+new Mugs() completed
+Mug(1)
+Mug(2)
+mug1 & mug2 initialized
+Mugs(int)
+new Mugs(1) completed
+*///:~
