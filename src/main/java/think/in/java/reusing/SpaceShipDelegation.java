b55bf06da96a348c0697ca4b4d248261ce7d6d77
limenghua <limenghua@wondersgroup.com>
2017-06-12 22:48:30 +0800

    继承

---
+++ src/main/java/think/in/java/reusing/SpaceShipDelegation.java
@@ -0,0 +1,49 @@
+package think.in.java.reusing;//: reusing/SpaceShipDelegation.java
+
+public class SpaceShipDelegation {
+    private String name;
+    /**
+     * 代理对象持有对被代理对象的引用
+     */
+    private SpaceShipControls controls =
+            new SpaceShipControls();
+
+    public SpaceShipDelegation(String name) {
+        this.name = name;
+    }
+
+    // Delegated methods:
+    public void back(int velocity) {
+        controls.back(velocity);
+    }
+
+    public void down(int velocity) {
+        controls.down(velocity);
+    }
+
+    public void forward(int velocity) {
+        controls.forward(velocity);
+    }
+
+    public void left(int velocity) {
+        controls.left(velocity);
+    }
+
+    public void right(int velocity) {
+        controls.right(velocity);
+    }
+
+    public void turboBoost() {
+        controls.turboBoost();
+    }
+
+    public void up(int velocity) {
+        controls.up(velocity);
+    }
+
+    public static void main(String[] args) {
+        SpaceShipDelegation protector =
+                new SpaceShipDelegation("NSEA Protector");
+        protector.forward(100);
+    }
+} ///:~
