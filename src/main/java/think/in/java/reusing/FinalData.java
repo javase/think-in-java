b6b8d26aec683e8b8c5018d0236878ebf0e1bf6b
limenghua <android.hua@gmail.com>
2017-07-18 08:53:30 +0800

    第7章 复用类 -final数据和空白final的使用

--- src/main/java/think/in/java/reusing/FinalData.java
+++ src/main/java/think/in/java/reusing/FinalData.java
@@ -13,6 +13,9 @@ class Value {
     }
 }
 
+/**
+ * final关键字探究
+ */
 public class FinalData {
     private static Random rand = new Random(47);
     private String id;
@@ -27,9 +30,12 @@ public class FinalData {
     // Typical public constant:
     public static final int VALUE_THREE = 39;
     // Cannot be compile-time constants:
+    // INT_5只有一份，new多个FinalData对象，这个变量只有一份
     private final int i4 = rand.nextInt(20);
     static final int INT_5 = rand.nextInt(20);
     private Value v1 = new Value(11);
+    // 引用类型指定为final
+    // final意味着无法将v2再次指向另一个新的对象
     private final Value v2 = new Value(22);
     private static final Value VAL_3 = new Value(33);
     // Arrays:
@@ -42,10 +48,19 @@ public class FinalData {
     public static void main(String[] args) {
         FinalData fd1 = new FinalData("fd1");
         //! fd1.valueOne++; // Error: can't change value
+        print("fd1.v2.i:" + fd1.v2.i);
         fd1.v2.i++; // Object isn't constant!
+
+        // final意味着无法将v2再次指向另一个新的对象
+        // 以下指定会报错  cannot assign a value to final variable 'v2'
+        // fd1.v2 = new Value(23);
+
+
+        print("fd1.v2.i++:" + fd1.v2.i);
         fd1.v1 = new Value(9); // OK -- not final
-        for (int i = 0; i < fd1.a.length; i++)
-            fd1.a[i]++; // Object isn't constant!
+        for (int i = 0; i < fd1.a.length; i++) {
+            fd1.a[i]++;
+        } // Object isn't constant!
         //! fd1.v2 = new Value(0); // Error: Can't
         //! fd1.VAL_3 = new Value(1); // change reference
         //! fd1.a = new int[3];
