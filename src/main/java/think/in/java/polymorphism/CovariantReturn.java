5920d0d071cbeb713fe182aac1cdf0821013449d
limenghua <limenghua@wondersgroup.com>
2017-10-31 22:02:51 +0800

    多态-状态模式，用组合来配合多态

---
+++ src/main/java/think/in/java/polymorphism/CovariantReturn.java
@@ -0,0 +1,43 @@
+package think.in.java.polymorphism;//: polymorphism/CovariantReturn.java
+
+class Grain {
+	public String toString() {
+		return "Grain";
+	}
+}
+
+class Wheat extends Grain {
+	public String toString() {
+		return "Wheat";
+	}
+}
+
+class Mill {
+	Grain process() {
+		return new Grain();
+	}
+}
+
+class WheatMill extends Mill {
+	Wheat process() {
+		return new Wheat();
+	}
+}
+
+/**
+ * 在导出类中的被覆盖方法可以返回  基类方法的返回类型的某种导出类型
+ */
+public class CovariantReturn {
+	public static void main(String[] args) {
+		Mill m = new Mill();
+		Grain g = m.process();
+		System.out.println(g);
+
+		m = new WheatMill();
+		g = m.process();
+		System.out.println(g);
+	}
+} /* Output:
+Grain
+Wheat
+*///:~
