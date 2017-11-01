07f25118024b7d31cf71d686663bce579f6153f0
limenghua <limenghua@wondersgroup.com>
2017-11-01 20:13:51 +0800

    接口-抽象类的构造器中调用动态绑定方法

---
+++ src/main/java/think/in/java/interfaces/music4/exercise/E01_Rodents.java
@@ -0,0 +1,91 @@
+package think.in.java.interfaces.music4.exercise;
+
+import static net.mindview.util.Print.print;
+
+/**
+ * 把普通的基类，改造成抽象类；
+ * 把原来基类里面被继承的方法，修改成抽象方法；
+ */
+abstract class Rodent {
+	public abstract void hop();
+
+	public abstract void scurry();
+
+	public abstract void reproduce();
+
+	public String toString() {
+		return "Rodent";
+	}
+}
+
+class Mouse extends Rodent {
+	public void hop() {
+		print("Mouse hopping");
+	}
+
+	public void scurry() {
+		print("Mouse scurrying");
+	}
+
+	public void reproduce() {
+		print("Making more Mice");
+	}
+
+	public String toString() {
+		return "Mouse";
+	}
+}
+
+class Gerbil extends Rodent {
+	public void hop() {
+		print("Gerbil hopping");
+	}
+
+	public void scurry() {
+		print("Gerbil scurrying");
+	}
+
+	public void reproduce() {
+		print("Making more Gerbils");
+	}
+
+	public String toString() {
+		return "Gerbil";
+	}
+}
+
+class Hamster extends Rodent {
+	public void hop() {
+		print("Hamster hopping");
+	}
+
+	public void scurry() {
+		print("Hamster scurrying");
+	}
+
+	public void reproduce() {
+		print("Making more Hamsters");
+	}
+
+	public String toString() {
+		return "Hamster";
+	}
+}
+
+
+public class E01_Rodents {
+	public static void main(String args[]) {
+		Rodent[] rodents = {
+				new Mouse(),
+				new Gerbil(),
+				new Hamster(),
+		};
+		for (Rodent r : rodents) {
+			r.hop();
+			r.scurry();
+			r.reproduce();
+			print(r);
+			print("-------");
+		}
+	}
+}
