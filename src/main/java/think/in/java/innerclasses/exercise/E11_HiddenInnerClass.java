35fcae18f761eb67f2e0a25a67ee1c3818cdd2ed
limenghua <limenghua@wondersgroup.com>
2018-02-25 18:40:29 +0800

    内部类-局部内部类、匿名内部类

---
+++ src/main/java/think/in/java/innerclasses/exercise/E11_HiddenInnerClass.java
@@ -0,0 +1,37 @@
+package think.in.java.innerclasses.exercise;
+
+import static net.mindview.util.Print.print;
+
+import think.in.java.innerclasses.exercise.exercise6.SimpleInterface;
+
+class Outer5 {
+
+	private class Inner implements SimpleInterface {
+		public void f() {
+			System.out.println("Outer5.Inner.f");
+		}
+	}
+
+	public SimpleInterface get() {
+		print("获得向上转型的对象");
+		return new Inner();
+	}
+
+	public Inner get2() {
+		print("直接获取内部类对象");
+		return new Inner();
+	}
+}
+
+public class E11_HiddenInnerClass {
+	public static void main(String args[]) {
+		Outer5 out = new Outer5();
+		SimpleInterface si = out.get();
+		// 只可以使用向上转型的类型，而不可直接使用私有内部类的类型，因为私有内部类对外部不可见
+		si = out.get2();
+		si.f();
+		// Won't compile -- 'Inner' not visible:
+		//! Inner i1 = out.get2();
+		//! Inner i2 = (Inner)si;
+	}
+} ///:~
---
+++ src/main/java/think/in/java/innerclasses/exercise/E12_AnonymousInnerClassAccess.java
@@ -0,0 +1,28 @@
+package think.in.java.innerclasses.exercise;
+
+public class E12_AnonymousInnerClassAccess {
+	private int i = 10;
+
+	private void f() {
+		System.out.println("E12_AnonymousInnerClassAccess.f");
+	}
+
+	public void h() {
+
+		new Object() {
+			void g() {
+				i++;
+				f();
+			}
+		}.g();
+		System.out.println("i = " + i);
+	}
+
+	public static void main(String args[]) {
+		E12_AnonymousInnerClassAccess ica = new E12_AnonymousInnerClassAccess();
+		ica.h();
+	}
+} /* Output:
+		 E12_AnonymousInnerClassAccess.f
+         i = 11
+         *///:~
