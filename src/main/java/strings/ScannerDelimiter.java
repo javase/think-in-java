a8d326e5c2175917bb07f3ea262ffafb8f101083
limenghua <limenghua@wondersgroup.com>
2019-08-05 12:54:31 +0800

    13 字符串 13.7.2 使用正则表达式扫描

--- src/main/java/strings/ScannerDelimiter.java
+++ src/main/java/strings/ScannerDelimiter.java
@@ -1,14 +1,23 @@
 package strings;//: strings/ScannerDelimiter.java
-import java.util.*;
 
+import java.util.Scanner;
+
+/**
+ * 默认情况下，Scanner根据空白字符对输入进行分词
+ * 可以使用正则表达式指定自己所需要的定界符
+ * /s 空白字符：[ \t\n\x0B\f\r]
+ * X* X，零次或多次
+ * 本例子中，使用的定界符是： 逗号前后任意的空白字符
+ */
 public class ScannerDelimiter {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner("12, 42, 78, 99, 42");
-    scanner.useDelimiter("\\s*,\\s*");
-    while(scanner.hasNextInt()) {
-		System.out.println(scanner.nextInt());
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner("12,    42, \t78, \n99, " +
+				"\n\n42");
+		scanner.useDelimiter("\\s*,\\s*");
+		while (scanner.hasNextInt()) {
+			System.out.println(scanner.nextInt());
+		}
 	}
-  }
 } /* Output:
 12
 42
--- src/main/java/strings/ThreatAnalyzer.java
+++ src/main/java/strings/ThreatAnalyzer.java
@@ -1,27 +1,36 @@
 package strings;//: strings/ThreatAnalyzer.java
-import java.util.regex.*;
-import java.util.*;
 
+import java.util.Scanner;
+import java.util.regex.MatchResult;
+
+/**
+ * 扫描一个防火墙日志文件中记录的威胁数据
+ * \\d: 数字：[0-9]
+ * X+: X，一次或多次
+ * X{n}：X，恰好 n 次
+ */
 public class ThreatAnalyzer {
-  static String threatData =
-    "58.27.82.161@02/10/2005\n" +
-    "204.45.234.40@02/11/2005\n" +
-    "58.27.82.161@02/11/2005\n" +
-    "58.27.82.161@02/12/2005\n" +
-    "58.27.82.161@02/12/2005\n" +
-    "[Next log section with different data format]";
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(threatData);
-    String pattern = "(\\d+[.]\\d+[.]\\d+[.]\\d+)@" +
-      "(\\d{2}/\\d{2}/\\d{4})";
-    while(scanner.hasNext(pattern)) {
-      scanner.next(pattern);
-      MatchResult match = scanner.match();
-      String ip = match.group(1);
-      String date = match.group(2);
-      System.out.format("Threat on %s from %s\n", date,ip);
-    }
-  }
+	static String threatData =
+			"58.27.82.161@02/10/2005\n" +
+					"204.45.234.40@02/11/2005\n" +
+					"58.27.82.161@02/11/2005\n" +
+					"58.27.82.161@02/12/2005\n" +
+					"58.27.82.161@02/12/2005\n" +
+					"[Next log section with different data format]";
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(threatData);
+		String pattern = "(\\d+[.]\\d+[.]\\d+[.]\\d+)@" +
+				"(\\d{2}/\\d{2}/\\d{4})";
+		while (scanner.hasNext(pattern)) {
+			scanner.next(pattern);
+			// 获取Match匹配结果
+			MatchResult match = scanner.match();
+			String ip = match.group(1);
+			String date = match.group(2);
+			System.out.format("Threat on %s from %s\n", date, ip);
+		}
+	}
 } /* Output:
 Threat on 02/10/2005 from 58.27.82.161
 Threat on 02/11/2005 from 204.45.234.40
