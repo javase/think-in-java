3b23786ba49b2799918658e724d718cd905f1488
limenghua <limenghua@wondersgroup.com>
2019-08-16 12:57:46 +0800

    14章 类型信息 14.2.2 泛型化的Class引用

--- src/main/java/typeinfo/BoundedClassReferences.java
+++ src/main/java/typeinfo/BoundedClassReferences.java
@@ -1,10 +1,13 @@
 package typeinfo;//: typeinfo/BoundedClassReferences.java
 
+/**
+ * 泛型 限定继承范围
+ */
 public class BoundedClassReferences {
-  public static void main(String[] args) {
-    Class<? extends Number> bounded = int.class;
-    bounded = double.class;
-    bounded = Number.class;
-    // Or anything else derived from Number.
-  }
+	public static void main(String[] args) {
+		Class<? extends Number> bounded = int.class;
+		bounded = double.class;
+		bounded = Number.class;
+		// Or anything else derived from Number.
+	}
 } ///:~
--- src/main/java/typeinfo/WildcardClassReferences.java
+++ src/main/java/typeinfo/WildcardClassReferences.java
@@ -1,8 +1,14 @@
 package typeinfo;//: typeinfo/WildcardClassReferences.java
 
+/**
+ * 泛型 通配符 ?
+ */
 public class WildcardClassReferences {
-  public static void main(String[] args) {
-    Class<?> intClass = int.class;
-    intClass = double.class;
-  }
+	public static void main(String[] args) {
+
+		// Class<Number> genericNumberClass = int.class; 会报错
+		// 因为 Integer Class 对象 不是 Number Class 对象的子类
+		Class<?> intClass = int.class;
+		intClass = double.class;
+	}
 } ///:~
